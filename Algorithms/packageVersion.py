6905d4447caee7b30b881f526289ce56986db210
Kiran Chandar Jayaraman <jkc.kiran@yahoo.co.in>
2019-10-02 20:56:59 +1000

    Create packageVersion.py

---
+++ Algorithms/packageVersion.py
@@ -0,0 +1,21 @@
+# This command is used to retrieve the version number of an already installed python package in your system
+# Below are few examples
+
+# scipy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
